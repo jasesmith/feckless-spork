9f18c674f900b023a8c5a61c7bf8f746c2159e2d
Jase <jase@star29.com>
2015-07-31 15:19:58 -0600

    some optimizing

--- directive/directive.js
+++ directive/directive.js
@@ -2,14 +2,6 @@
     'use strict';
 
     $angular.module('app').directive('spork', ['$window', function spork($window) {
-        var getAngle = function(x1, y1, x2, y2) {
-            var dy = y2 - y1;
-            var dx = x2 - x1;
-            return Math.atan2(dy, dx) * 180 / Math.PI; // range (-PI, PI]
-            // theta *= 180 / Math.PI; // rads to degs, range (-180, 180]
-            // if (theta < 0) theta = 360 + theta; // range [0, 360)
-            // return theta;
-        }
 
         var getNumbers = function(target){
             var numbers = {};
@@ -22,13 +14,48 @@
                     w: Math.floor(target.clientWidth),
                     h: Math.floor(target.clientHeight),
                 };
+                // find x|y center
                 numbers.cx = (numbers.l + (numbers.h/2));
                 numbers.cy = (numbers.t + (numbers.w/2));
-
-                // numbers.x = getXpos(pos, numbers);
-                // numbers.y = getYpos(pos, numbers);
             }
             return numbers;
+        };
+
+        var getMetrics = function(el, parent, rtl) {
+            rtl = rtl || false;
+
+            var en = getNumbers(el);
+            var pn = getNumbers(parent);
+
+            var p1 = {
+                x: en.cx,
+                y: en.cy
+            };
+            var p2 = {
+                x: pn.cx,
+                y: pn.cy
+            };
+
+            // angle in radians
+            var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+
+            // angle in degrees
+            var angleDegrees = angleRadians * 180 / Math.PI;
+
+            if(rtl) {
+                // this because if the line orientation is right to left
+                angleDegrees += 180;
+            }
+
+            // length of line between two points
+            // last operation as this alters the number set
+            var lineLength = Math.sqrt(((p1.x -= p2.x) * p1.x) + ((p1.y -= p2.y) * p1.y));
+
+            return {
+                lineLength: lineLength,
+                angleRadians: angleRadians,
+                angleDegrees: angleDegrees
+            };
         };
 
         return {
@@ -41,44 +68,22 @@
 
             controller: ['$scope', function controller($scope) {
 
-                $scope.lineLength = function lineLength(el, parent) {
-                    el = $('#node-' + el);
-                    parent = $('#node-' + parent);
+                $scope.connector = function connector(el, parent) {
+                    el = $angular.element('#node-' + el)[0];
+                    parent = $angular.element('#node-' + parent)[0];
 
-                    var en = getNumbers(el[0]);
-                    var pn = getNumbers(parent[0]);
-                    var p1 = {
-                    	x: en.cx,
-                    	y: en.cy
-                    };
-                    var p2 = {
-                    	x: pn.cx,
-                    	y: pn.cy
-                    };
-
-                    // angle in radians
-                    var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
-
-                    // angle in degrees
-                    var angleDeg = Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
-                    // this because we are working the lines right to left
-                    angleDeg += 180;
-
-                    var l = Math.sqrt(((p1.x -= p2.x) * p1.x) + ((p1.y -= p2.y) * p1.y));
-                    // d = getAngle(x1, y1, x2, y2);
-
-                    window.console.log(l, angleDeg);
+                    var metrics = getMetrics(el, parent, true);
 
                     return {
-                        width: l + 'px',
-                        transform: 'translateY(-50%) rotate(' + angleDeg + 'deg)'
+                        width: metrics.lineLength + 'px',
+                        transform: 'translateY(-50%) rotate(' + metrics.angleDegrees + 'deg)'
                     };
                 };
 
             }],
 
             template: '' +
-                '<div class="spork {{config.style}}">' +
+                '<div class="spork">' +
                     '<ul>' +
                         '<li ng-repeat="a in model">' +
                             '<div id="node-{{a.id}}">' +
@@ -89,21 +94,21 @@
                                 '<li ng-repeat="b in a.children">' +
                                     '<div id="node-{{b.id}}">' +
                                         '<div class="node"><span class="label">{{b.id}}</span></div>' +
-                                        '<div class="line" ng-style="lineLength(b.id, a.id)"></div>' +
+                                        '<div class="line" ng-style="connector(b.id, a.id)"></div>' +
                                     '</div>' +
 
                                     '<ul ng-if="b.children.length">' +
                                         '<li ng-repeat="c in b.children">' +
                                             '<div id="node-{{c.id}}">' +
                                                 '<div class="node"><span class="label">{{c.id}}</span></div>' +
-                                                '<div class="line" ng-style="lineLength(c.id, b.id)"></div>' +
+                                                '<div class="line" ng-style="connector(c.id, b.id)"></div>' +
                                             '</div>' +
 
                                             '<ul ng-if="c.children.length">' +
                                                 '<li ng-repeat="d in c.children">' +
                                                     '<div id="node-{{d.id}}">' +
                                                         '<div class="node"><span class="label">{{d.id}}</span></div>' +
-                                                        '<div class="line" ng-style="lineLength(d.id, c.id)"></div>' +
+                                                        '<div class="line" ng-style="connector(d.id, c.id)"></div>' +
                                                     '</div>' +
 
                                                 '</li>' +
